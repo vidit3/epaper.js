92b5d20ab115152f96def882d261c1403d511e3d
vidit-kumar_ghub <vidit.kumar@blueyonder.com>
2023-07-15 13:04:59 +0530

    Added initial support for RPI 7in5bV2

--- packages/cli/src/deviceFactory.ts
+++ packages/cli/src/deviceFactory.ts
@@ -19,6 +19,7 @@ const deviceMap = new Map<string, (orientation?: Orientation, colorMode?: ColorM
     ['rpi-7in5-v2', getRpi7in5V2],
     ['rpi-2in7', getRpi2In7],
     ['rpi-3in7', getRpi3In7],
+    ['rpi-7in5b-v2', getRpi7in5bV2],
 ]);
 
 async function getRpi2in13V2(orientation?: Orientation, colorMode?: ColorMode): Promise<DisplayDevice> {
@@ -72,5 +73,14 @@ async function getRpi3In7(orientation?: Orientation, colorMode?: ColorMode): Pro
         return new Rpi3In7(orientation, colorMode);
     } catch (e) {
         throw new Error('Failed to import @epaperjs/rpi-3in7, make sure it is installed');
+    }
+}
+
+async function getRpi7in5bV2(orientation?: Orientation, colorMode?: ColorMode): Promise<DisplayDevice> {
+    try {
+        const { Rpi7In5bV2 } = await import('@epaperjs/rpi-7in5b-v2');
+        return new Rpi7In5bV2(orientation, colorMode);
+    } catch (e) {
+        throw new Error('Failed to import @epaperjs/rpi-7in5b-v2, make sure it is installed');
     }
 }
---
+++ packages/rpi-7in5b-v2/README.md
@@ -0,0 +1,17 @@
+# @epaperjs/rpi-7in5b-v2
+
+Node bindings for [Waveshare 7.5 "](https://www.waveshare.com/7.5inch-e-paper-hat-b.htm)
+
+## Supported Color Modes
+
+`Black / White`
+
+## Installation
+
+```
+npm i -g @epaperjs/rpi-7in5b-v2
+```
+
+## License
+
+MIT
